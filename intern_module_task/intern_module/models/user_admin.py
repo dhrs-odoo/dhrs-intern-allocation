9e3a04fdb5793b6eb3806e27c3edf7984cf8a340
dhrs-odoo <dhrs@odoo.com>
2023-01-11 19:14:58 +0530

    commits

--- intern_module_task/intern_module/models/user_admin.py
+++ intern_module_task/intern_module/models/user_admin.py
@@ -9,10 +9,11 @@ class internDesk(models.Model):
      cllg_name = fields.Char(string='College Name',required = True)
      contact_details = fields.Char(string = 'Contact No',required=True)
      joining_date = fields.Date(string = 'Joining Date')
-     equipment_type = fields.Many2many("equipment.allocation" , string='Equipments')
+     Laptop_assigned = fields.Many2one("equipment.allocation" , string='Laptop Assigned')
      intern_type = fields.Many2one("role.allocation",string='Intern Role')
      description = fields.Char(string='Description')
      desk_no=fields.Many2one("desk.allocation", string="Desk No")
+     other_equipments = fields.Many2many("equipment.allocation" , string="Other Equipments")
      
      
      state = fields.Selection(selection = [('new','New'),('allocated','Allocated')],default='new')
