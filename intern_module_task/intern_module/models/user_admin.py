10110b34e35f0b9fe5fd1b475d59005cc93979bb
dhrs-odoo <dhrs@odoo.com>
2023-01-18 10:57:02 +0530

    added new fields and chatter

--- intern_module_task/intern_module/models/user_admin.py
+++ intern_module_task/intern_module/models/user_admin.py
@@ -11,32 +11,41 @@ class internDesk(models.Model):
      cllg_name = fields.Char(string='College Name',required = True)
      contact_details = fields.Char(string = 'Contact No',required=True)
      joining_date = fields.Date(string = 'Joining Date')
-     laptop_assigned = fields.Many2one("laptop.allocation" , string='Laptop Assigned')
+     laptop_assigned = fields.Many2one("laptop.allocation" , string='Laptop Assigned' , tracking = True)
      intern_type = fields.Many2one("role.allocation",string='Intern Role')
      description = fields.Char(string='Description')
      desk_no=fields.Many2one("desk.allocation", string="Desk No")
      other_equipments = fields.Many2many("equipment.allocation" , string="Other Equipments")
      email_id = fields.Char(string = 'Email-Id')
-     coach = fields.Many2one("functional.training",  string = "Coach Name")
+     coach = fields.Many2one("functional.training",  string = "Coach Name" , tracking = True)
+     assigned_date = fields.Date(string = "Assigned Date",default = fields.Datetime.now())
      
      
-     state = fields.Selection(selection = [('pending','Pending'),('allocated','Allocated')],default='pending',tracking = True)
-     status = fields.Selection(selection = [('allocated','Allocated'),('pending','Pending')])
+     state = fields.Selection(selection = [('new','New'),('functional','Functional Training'),('backend','Odoo Backend'),('frontend','Odoo Frontend'),('owl','Owl JS'),('done','Done')],tracking = True)
+     status = fields.Selection(selection = [('allocated','Allocated')])
      
      _sql_constraints = [('name_unique','UNIQUE(name)','Name must be Unique')]
   
      
+     # def allocated_button(self):
+     #      for record in self:
+     #                record.status='allocated'
+     #                record.state='allocated'
+     
      def allocated_button(self):
           for record in self:
-                    record.status='allocated'
-                    record.state='allocated'
-     
-     def pending_button(self):
-          for record in self:
-               if record.status == 'allocated':
-                    raise UserError('You have already allocated the interns')
+               if record.desk_no:
+                    # record.status='allocated'
+                    record.state='new'
                else :
-                    record.status == 'pending'
-                    record.state  == 'pending'
+                    raise UserError('You need to assign seat no first')
+                    
+     # def pending_button(self):
+     #      for record in self:
+     #           if record.status == 'allocated':
+     #                raise UserError('You have already allocated the interns')
+     #           else :
+     #                record.status == 'pending'
+     #                record.state  == 'pending'
 
      
